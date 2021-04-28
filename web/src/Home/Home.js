d2383a44a54a87bec3a90ebc1e27c68dc2743a04
codeafix <matt.c.buckley@gmail.com>
2021-04-28 21:59:50 +0100

    Add modal for adding a sim

--- web/src/Home/Home.js
+++ web/src/Home/Home.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import API from '../api';
 import SimList from './SimList';
+import Modal from 'react-bootstrap/Modal'
 
 const Home = () => {
     const [simlist, setSimlist] = useState([]);
     const [notes, setNotes] = useState("");
-    
+    const [showmodal, setShowmodal] = useState(false);
+    const handleClose = () => setShowmodal(false);
+    const handleShow = () => setShowmodal(true);  
+
     useEffect(() => {
         API.sims()
             .then(response => {
@@ -15,10 +19,11 @@ const Home = () => {
             })
       },[]);
 
-    function addSimulation(){
+    const addSimulation = () => {
         API.add().then(response => {
             setSimlist(simlist.concat(response));
         });
+        handleClose();
     }
     
     return(
@@ -27,7 +32,24 @@ const Home = () => {
             <p>{notes}</p>
             <h2>List of Simulations</h2>
             <SimList sims={simlist}/>
-            <button onClick={addSimulation}>Add</button>
+            <button onClick={handleShow}>Add</button>
+            <Modal
+                show={showmodal}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Simulation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Add a new simulation
+                </Modal.Body>
+                <Modal.Footer>
+                    <button variant="secondary" onClick={handleClose}>Close</button>
+                    <button variant="primary" onClick={addSimulation}>Add</button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
